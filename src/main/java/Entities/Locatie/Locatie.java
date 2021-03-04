826a3ee7d8a163437f542a44acebf74bfef420c6
ecs.emanuel <ecs.emanuel@yahoo.com>
2021-03-04 18:43:43 +0200

    rewrote Locatie entity class

--- src/main/java/Entities/Locatie/Locatie.java
+++ src/main/java/Entities/Locatie/Locatie.java
@@ -3,17 +3,32 @@ package Entities.Locatie;
 public class Locatie
 {
     private int indexLocatie;
-
     private Judet judetLocatie;
+    private Oras orasLocatie;
+    private Cartier cartierLocatie;
+    private Comuna comunaLocatie;
+    private Sat satLocatie;
     private String denumireLocatie;
 
     public Locatie()
     {
     }
 
-    public Locatie(int indexLocatie, String denumireLocatie)
+    public Locatie(int indexLocatie, Judet judetLocatie, Oras orasLocatie, Cartier cartierLocatie, String denumireLocatie)
     {
         this.indexLocatie = indexLocatie;
+        this.judetLocatie = judetLocatie;
+        this.orasLocatie = orasLocatie;
+        this.cartierLocatie = cartierLocatie;
+        this.denumireLocatie = denumireLocatie;
+    }
+
+    public Locatie(int indexLocatie, Judet judetLocatie, Comuna comunaLocatie, Sat satLocatie, String denumireLocatie)
+    {
+        this.indexLocatie = indexLocatie;
+        this.judetLocatie = judetLocatie;
+        this.comunaLocatie = comunaLocatie;
+        this.satLocatie = satLocatie;
         this.denumireLocatie = denumireLocatie;
     }
 
@@ -27,6 +42,56 @@ public class Locatie
         this.indexLocatie = indexLocatie;
     }
 
+    public Judet getJudetLocatie()
+    {
+        return judetLocatie;
+    }
+
+    public void setJudetLocatie(Judet judetLocatie)
+    {
+        this.judetLocatie = judetLocatie;
+    }
+
+    public Oras getOrasLocatie()
+    {
+        return orasLocatie;
+    }
+
+    public void setOrasLocatie(Oras orasLocatie)
+    {
+        this.orasLocatie = orasLocatie;
+    }
+
+    public Cartier getCartierLocatie()
+    {
+        return cartierLocatie;
+    }
+
+    public void setCartierLocatie(Cartier cartierLocatie)
+    {
+        this.cartierLocatie = cartierLocatie;
+    }
+
+    public Comuna getComunaLocatie()
+    {
+        return comunaLocatie;
+    }
+
+    public void setComunaLocatie(Comuna comunaLocatie)
+    {
+        this.comunaLocatie = comunaLocatie;
+    }
+
+    public Sat getSatLocatie()
+    {
+        return satLocatie;
+    }
+
+    public void setSatLocatie(Sat satLocatie)
+    {
+        this.satLocatie = satLocatie;
+    }
+
     public String getDenumireLocatie()
     {
         return denumireLocatie;
@@ -35,11 +100,6 @@ public class Locatie
     public void setDenumireLocatie(String denumireLocatie)
     {
         this.denumireLocatie = denumireLocatie;
-    }
-
-    public boolean hasInformationComplete()
-    {
-        return indexLocatie > 0 && !denumireLocatie.isEmpty();
     }
 }
 
