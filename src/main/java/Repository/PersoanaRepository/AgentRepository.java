deb902112fb8793a3e50b098ef5e7843a7f19507
ecs.emanuel <ecs.emanuel@yahoo.com>
2021-03-08 02:44:29 +0200

    getAgent function now returns a complete Agent entity

--- src/main/java/Repository/PersoanaRepository/AgentRepository.java
+++ src/main/java/Repository/PersoanaRepository/AgentRepository.java
@@ -1,12 +1,11 @@
 package Repository.PersoanaRepository;
 
+import Entities.Locatie.*;
 import Repository.DatabaseRepository;
 import Utils.QueryOutcome;
 import Entities.Persoana.Agent;
 import Entities.Persoana.User;
 import javafx.util.Pair;
-
-import javax.sql.rowset.CachedRowSet;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -26,11 +25,32 @@ public class AgentRepository
         {
             return new Pair<>(agent, QueryOutcome.OFFLINE);
         }
-        
+
         String sqlScript = String.format
         (
-            "SELECT * " +
-            "FROM agenti " +
+            "SELECT a.indexAgent,\n" +
+            "p.indexPersoana, p.numePersoana, p.prenumePersoana, p.telefonPersoana, p.emailPersoana, \n" +
+            "l.indexLocatie, l.denumireLocatie,\n" +
+            "j.indexJudet, j.denumireJudet, \n" +
+            "o.indexOras, o.denumireOras, \n" +
+            "ca.indexCartier, ca.denumireCartier,\n" +
+            "co.indexComuna, co.denumireComuna, \n" +
+            "s.indexSat, s.denumireSat\n" +
+            "FROM agenti a \n" +
+            "JOIN persoane p \n" +
+            "ON a.persoanaAgent = p.indexPersoana\n" +
+            "JOIN locatii l \n" +
+            "ON p.domiciliuPersoana = l.indexLocatie\n" +
+            "JOIN judete j \n" +
+            "ON l.judetLocatie = j.indexJudet\n" +
+            "LEFT JOIN orase o \n" +
+            "ON l.orasLocatie = o.indexOras\n" +
+            "LEFT JOIN cartiere ca\n" +
+            "ON l.cartierLocatie = ca.indexCartier\n" +
+            "LEFT JOIN comune co \n" +
+            "ON l.comunaLocatie = co.indexComuna\n" +
+            "LEFT JOIN sate s \n" +
+            "ON l.satLocatie = s.indexSat\n" +
             "WHERE userAgent = %d",
             user.getIndexUser()
         );
@@ -39,11 +59,54 @@ public class AgentRepository
         {
             try (ResultSet resultset = statament.executeQuery(sqlScript))
             {
-                // user found in database
+                // agent found in database
                 if (resultset.first())
                 {
+                    System.out.println("test");
                     agent.setIndexAgent(resultset.getInt(1));
                     agent.setUserAgent(user);
+                    agent.setIndexPersoana(resultset.getInt(2));
+                    agent.setNumePersoana(resultset.getString(3));
+                    agent.setPrenumePersoana(resultset.getString(4));
+                    agent.setTelefonPersoana(resultset.getString(5));
+                    agent.setEmailPersoana(resultset.getString(6));
+
+                    Locatie domiciliu = new Locatie();
+                    domiciliu.setIndexLocatie(resultset.getInt(7));
+                    domiciliu.setDenumireLocatie(resultset.getString(8));
+
+                    Judet judet = new Judet();
+                    judet.setIndexJudet(resultset.getInt(9));
+                    judet.setDenumireJudet(resultset.getString(10));
+                    domiciliu.setJudetLocatie(judet);
+
+                    if (resultset.getInt(11) > 0)
+                    {
+                        Oras oras = new Oras();
+                        oras.setIndexOras(resultset.getInt(11));
+                        oras.setDenumireOras(resultset.getString(12));
+                        domiciliu.setOrasLocatie(oras);
+
+                        Cartier cartier = new Cartier();
+                        cartier.setIndexCartier(resultset.getInt(13));
+                        cartier.setDenumireCartier(resultset.getString(14));
+                        domiciliu.setCartierLocatie(cartier);
+                    }
+                    else if (resultset.getInt(15) > 0)
+                    {
+                        Comuna comuna = new Comuna();
+                        comuna.setIndexComuna(resultset.getInt(15));
+                        comuna.setDenumireComuna(resultset.getString(16));
+                        domiciliu.setComunaLocatie(comuna);
+
+                        Sat sat = new Sat();
+                        sat.setIndexSat(resultset.getInt(17));
+                        sat.setDenumireSat(resultset.getString(18));
+                        domiciliu.setSatLocatie(sat);
+                    }
+
+                    agent.setDomiciliuPersoana(domiciliu);
+
                     return new Pair<>(agent, QueryOutcome.SUCCESS);
                 }
 
