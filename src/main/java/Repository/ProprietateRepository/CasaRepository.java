78ebc30299b4c35062417bda8b93cd2500b8a2d5
ecs.emanuel <ecs.emanuel@yahoo.com>
2021-03-09 05:11:58 +0200

    added addCasa function (Repository + Services)

---
+++ src/main/java/Repository/ProprietateRepository/CasaRepository.java
@@ -0,0 +1,143 @@
+package Repository.ProprietateRepository;
+
+import Entities.Locatie.Locatie;
+import Entities.Proprietate.*;
+import Repository.DatabaseRepository;
+import Utils.QueryOutcome;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CasaRepository
+{
+    public QueryOutcome addCasa(Casa casa)
+    {
+        DatabaseRepository databaseRepository = new DatabaseRepository();
+        Connection connection = databaseRepository.createConnection();
+
+        if (connection == null)
+        {
+            return QueryOutcome.OFFLINE;
+        }
+
+        try (Statement statement = connection.createStatement())
+        {
+            connection.setAutoCommit(false);
+
+            // add locatie
+            String sqlScript1 = "";
+            Locatie locatie = casa.getLocatieProprietate();
+
+            if (locatie.getOrasLocatie() != null)
+            {
+                sqlScript1 = String.format
+                (
+                "INSERT INTO locatii (judetLocatie, orasLocatie, cartierLocatie, denumireLocatie) VALUES\n" +
+                "(%d, %d, %d, '%s');\n",
+                locatie.getJudetLocatie().getIndexJudet(),
+                locatie.getOrasLocatie().getIndexOras(), locatie.getCartierLocatie().getIndexCartier(), locatie.getDenumireLocatie()
+                );
+            }
+            else
+            {
+                sqlScript1 = String.format
+                (
+                "INSERT INTO locatii (judetLocatie, comunaLocatie, satLocatie, denumireLocatie) VALUES\n" +
+                "(%d, %d, %d, '%s');\n",
+                locatie.getJudetLocatie().getIndexJudet(),
+                locatie.getComunaLocatie().getIndexComuna(), locatie.getSatLocatie().getIndexSat(), locatie.getDenumireLocatie()
+                );
+            }
+
+            int indexLocatie = statement.executeUpdate(sqlScript1, Statement.RETURN_GENERATED_KEYS);
+
+            // add proprietate
+            String sqlScript2 = String.format
+            (
+                "INSERT INTO proprietati (titluProprietate, descriereProprietate, pretProprietate,\n" +
+                "locatieProprietate, proprietarProprietate, agentProprietate,\n" +
+                "dispozitieProprietate, dataProprietate) VALUES\n" +
+                "('%s', '%s', %d, %d, %d, %d, '%s', '%s');",
+                casa.getTitluProprietate(), casa.getDescriereProprietate(), casa.getPretProprietate(),
+                indexLocatie, casa.getProprietarProprietate().getIndexClient(), casa.getAgentProprietate().getIndexAgent(),
+                casa.getDispozitieProprietate().name(), casa.getDataProprietate()
+            );
+
+            // retrieve indexProprietate for later use
+            int indexProprietate = statement.executeUpdate(sqlScript2, Statement.RETURN_GENERATED_KEYS);
+
+            // add parcela
+            Constructie constructie = new Constructie();
+            Parcela parcela = constructie.getParcelaConstructie();
+
+            String sqlScript3 = String.format
+            (
+                "INSERT INTO parcele (suprafataParcela, hasApa, hasGaz, hasElectricitate, hasCanalizare) VALUES\n" +
+                "(%d, %b, %b, %b, %b);",
+                parcela.getSuprafataParcela(), parcela.hasApa(), parcela.hasGaz(), parcela.hasElectricitate(), parcela.hasCanalizare()
+            );
+
+            // retrieve indexParcela
+            int indexParcela = statement.executeUpdate(sqlScript3, Statement.RETURN_GENERATED_KEYS);
+
+            // add compartimentare
+            Compartimentare compartimentare = constructie.getCompartimentareConstructie();
+
+            String sqlScript4 = String.format
+            (
+                "INSERT INTO compartimentari(openSpace, living, dormitor, dressing,\n" +
+                "bucatarie, debara, baie, hol, mansarda, balcon, terasa, gradina, parcare, garaj, boxa, pod) VALUES\n" +
+                "(%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d",
+                compartimentare.getOpenspace(), compartimentare.getLiving(),
+                compartimentare.getDormitor(), compartimentare.getDressing(),
+                compartimentare.getBucatarie(), compartimentare.getDebara(),
+                compartimentare.getBaie(), compartimentare.getHol(),
+                compartimentare.getMansarda(), compartimentare.getBalcon(),
+                compartimentare.getTerasa(), compartimentare.getGradina(),
+                compartimentare.getParcare(), compartimentare.getGaraj(),
+                compartimentare.getBoxa(), compartimentare.getPod()
+            );
+
+            statement.addBatch(sqlScript4);
+
+            String sqlScript5 = String.format
+            (
+                "INSERT INTO constructii (suprafataUtilizabila, suprafataConstructie,\n" +
+                "inaltimeConstructie, anConstructie, structuraConstructie\n" +
+                "dispozitieActuala, dispozitiePredare,\n" +
+                "compartimentareConstructie, parcelaConstructie) VALUES\n" +
+                "(%d, %d, %d, %d, '%s', '%s', '%s', LAST_INSERT_ID(), %d)",
+                constructie.getSuprafataUtilizabila(), constructie.getSuprafataConstructie(),
+                constructie.getInaltimeConstructie(), constructie.getAnConstructie(), constructie.getStructuraConstructie().name(),
+                constructie.getDispozitieActuala().name(), constructie.getDispozitiePredare().name(), indexParcela
+            );
+
+            statement.addBatch(sqlScript5);
+
+            String sqlScript6 = String.format
+            (
+                "INSERT INTO casute (constructieCasa, proprietateCasa) VALUES\n" +
+                "(LAST_INSERT_ID(), %d",
+                indexProprietate
+            );
+
+            statement.addBatch(sqlScript6);
+            statement.executeBatch();
+            connection.commit();
+
+            return QueryOutcome.SUCCESS;
+        }
+        catch (SQLException throwables)
+        {
+            throwables.printStackTrace();
+        }
+        finally
+        {
+            databaseRepository.closeConnection(connection);
+        }
+
+        return QueryOutcome.ERROR;
+    }
+}
